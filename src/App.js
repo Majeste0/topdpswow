823db17df99da5e5dbc336a74202c5a48c0ea061
a <a>
2022-07-23 09:05:56 +0200

    Setup CRA

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-full">
+      <div className="w-full h-[100px] text-black">Test</div>;
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/mainPage/header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const header = () => {
+  return <div className="w-full h-[600px] text-blue"></div>;
+};
+
+export default header;
