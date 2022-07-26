3c4960bb1a305eb9ae407ac9de03519bda5be496
a <a>
2022-07-26 09:53:05 +0200

    Ajout des useState pour le zoom/opacité sur le main

--- src/Components/mainPage/header.js
+++ src/Components/mainPage/header.js
@@ -3,8 +3,8 @@ import React from "react";
 const Header = () => {
   return (
     <>
-      <div className="w-full ">
-        <h1 className=" font-chakra text-4xl text-center tracking-[-0.1em] text-blanc pt-[10px] font-bold">
+      <div className="w-full cursor-default ">
+        <h1 className=" font-chakra text-4xl text-center tracking-[-0.1em] text-blanc pt-[10px] font-bold transition ease-in-out delay-150 hover:scale-110 duration-300">
           TOP HPS
         </h1>
         <h2 className="text-center font-poppins text-blanc text-md pb-[5px] ">
@@ -12,7 +12,7 @@ const Header = () => {
           Je peux avoir l'innerv stp ?
         </h2>
       </div>
-      <div className="h-[2px] w-full bg-doré absolute"></div>
+      <div className="h-[2px] w-full bg-doré absolute z-10"></div>
     </>
   );
 };
--- src/Components/mainPage/main.js
+++ src/Components/mainPage/main.js
@@ -1,40 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import df_wp from "../../images/dragonflight_wp.jpg";
 import heal_wp from "../../images/heal_wp.webp";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlay } from "@fortawesome/free-solid-svg-icons";
 
 const Main = () => {
+  const [isShown, setIsShown] = useState(false);
+  const [isShown2, setIsShown2] = useState(false);
+
   return (
     <>
-      <div className="flex h-full justify-around">
-        <div className="relative w-full">
-          <img
-            src={df_wp}
-            alt="dfwp"
-            className="w-full h-full object-cover opacity-50"
-          />
-
-          <h3 className="text-white absolute text-center w-full top-[40%] font-goldman text-2xl">
-            GUIDES DE CLASSE
-          </h3>
-          <p className="text-white absolute w-[50%] left-[25%] top-[45%] text-center font-poppins text-base">
-            Apprend à maîtriser ton personnage sur le bout des doigts
-          </p>
+      <div className="flex h-full justify-around overflow-hidden">
+        <div className="relative w-full cursor-pointer overflow-hidden">
+          <div
+            className="h-full"
+            onMouseEnter={() => setIsShown(true)}
+            onMouseLeave={() => setIsShown(false)}
+          >
+            <img
+              src={df_wp}
+              alt="healwp"
+              className={
+                isShown === true
+                  ? "w-full h-full object-cover cursor-pointer transition ease-in-out scale-110 duration-300"
+                  : "w-full h-full object-cover cursor-pointer opacity-50"
+              }
+            />
+            <div>
+              <h3 className="cursor-pointer text-white absolute text-center w-full top-[40%] font-goldman text-2xl ">
+                GUIDES DE CLASSE
+              </h3>
+              <p className="cursor-pointer text-white absolute w-[50%] left-[25%] top-[45%] text-center font-poppins text-base">
+                Apprend à maîtriser ton personnage sur le bout des doigts
+              </p>
+            </div>
+          </div>
         </div>
-        <div className="w-[2px] h-full bg-doré fixed"></div>;
-        <div className="w-full relative">
-          <img
-            src={heal_wp}
-            alt="healwp"
-            className="w-full h-full object-cover opacity-50"
-          />
-          <h3 className="text-white absolute text-center w-full top-[40%] font-goldman text-2xl">
-            OPTIMISATIONS
-          </h3>
-          <p className="text-white absolute w-[50%] left-[25%] top-[45%] text-center font-poppins text-base">
-            Découvre des astuces pour t'optimiser avant de partir en instances
-          </p>
+        <div className="w-[2px] h-full bg-doré fixed z-1"></div>;
+        <div
+          className="w-full relative cursor-pointer overflow-hidden "
+          onMouseEnter={() => setIsShown2(true)}
+          onMouseLeave={() => setIsShown2(false)}
+        >
+          <div className="w-full h-full  ">
+            <img
+              src={heal_wp}
+              alt="healwp"
+              className={
+                isShown2 === true
+                  ? "w-full h-full object-cover cursor-pointer transition ease-in-out scale-110 duration-300"
+                  : "w-full h-full object-cover cursor-pointer opacity-50"
+              }
+            />
+            <div>
+              <h3 className="cursor-pointer text-white absolute text-center w-full top-[40%] font-goldman text-2xl">
+                OPTIMISATIONS
+              </h3>
+              <p className="cursor-pointer text-white absolute w-[50%] left-[25%] top-[45%] text-center font-poppins text-base">
+                Découvre des astuces pour t'optimiser avant de partir en
+                instances
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </>
